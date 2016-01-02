5dac6deb24763f4a7c1a8575f0ab57b302da9997
Jon Zielenkievicz <jonzielen@gmail.com>
2016-01-02 17:43:25 -0500

    update blog post list items

--- public/controllers/blogPostsController.js
+++ public/controllers/blogPostsController.js
@@ -3,16 +3,16 @@ var siteApp = angular.module('siteApp', ['ngAnimate']);
 siteApp.controller('BlogPostsCtrl', function($scope, $http) {
     $http.get('feeds/blog').success(function(data) {
 
-        function tagsArray(tags) {
+        function splitTagsToArray(tags) {
             return tags.split(', ');
         };
 
-        data.map(function(obj) {
-            obj.tags = tagsArray(obj.tags)
-        });
+        function getTags(obj) {
+            obj.tags = splitTagsToArray(obj.tags)
+        };
+
+        data.map(getTags);
 
         $scope.posts = data;
-
-        //console.log(data);
     });
 });
