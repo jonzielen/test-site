2ddb316fb89e6ba59f5c86001545e04d703067be
Jon Zielenkievicz <jonzielen@gmail.com>
2016-01-02 17:30:57 -0500

    clean up blog post page

--- public/controllers/blogPostsController.js
+++ public/controllers/blogPostsController.js
@@ -2,6 +2,17 @@ var siteApp = angular.module('siteApp', ['ngAnimate']);
 
 siteApp.controller('BlogPostsCtrl', function($scope, $http) {
     $http.get('feeds/blog').success(function(data) {
+
+        function tagsArray(tags) {
+            return tags.split(', ');
+        };
+
+        data.map(function(obj) {
+            obj.tags = tagsArray(obj.tags)
+        });
+
         $scope.posts = data;
+
+        //console.log(data);
     });
 });
