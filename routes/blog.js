9d9f9cf021af5dcb4f31765ae3b267aa86ae1fb7
Jon Zielenkievicz <jonzielen@gmail.com>
2016-01-02 17:07:36 -0500

    update blog post page

--- routes/blog.js
+++ routes/blog.js
@@ -1,18 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET blog page. */
-router.get('/', function(req, res, next) {
-  res.render('blog-page', {
-      title: 'Blog Page'
-  });
-});
+var db = require('../app/db');
 
 /* GET blog page. */
-router.get('/code-example', function(req, res, next) {
-  res.render('blog-page', {
-      title: 'need to figure this out'
-  });
+router.get('/', function(req, res, next) {
+    res.render('blog-page', {
+        title: 'Blog Page'
+    });
 });
+
+/* GET blog post page. */
+router.get('/:url', function(req, res, next) {
+    var url = req.params.url,
+        collection = db.get().collection('blogPosts');
+
+    collection.findOne({"url":url}, function(err, post) {
+        res.render('blogPost-page', {
+            meta: post.meta,
+            title: post.title,
+            date: post.date,
+            tags: post.tags,
+            body: post.body
+        });
+    });
+});
+
+// /* GET blog tags page. */
+// router.get('/tags', function(req, res, next) {
+//     res.render('blogTags-page', {
+//         title: 'need to figure this out'
+//     });
+// });
+
+// /* GET blog page. */
+// router.get('/code-example', function(req, res, next) {
+//     res.render('blog-page', {
+//         title: 'need to figure this out'
+//     });
+// });
 
 module.exports = router;
