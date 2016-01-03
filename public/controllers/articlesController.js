c5603658c513f388f730e7b09ff281fcaf8755d2
Jon Zielenkievicz <jonzielen@gmail.com>
2016-01-03 13:35:57 -0500

    clean up

--- public/controllers/articlesController.js
+++ public/controllers/articlesController.js
@@ -2,6 +2,15 @@ var siteApp = angular.module('siteApp', ['ngAnimate']);
 
 siteApp.controller('ArticlesCtrl', function($scope, $http) {
     $http.get('feeds/article').success(function(data) {
+
+        function splitTagsToArray(tags) {
+            return tags.split(', ');
+        };
+
+        data.forEach(function(obj) {
+            obj.tags = splitTagsToArray(obj.tags);
+        });
+        
         $scope.articles = data;
     });
 });
